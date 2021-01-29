0663f2859a42df2837cce4ba94e28a62d69ac47a
PakVu <bach.x.vu@eastagile.com>
2021-01-29 11:06:54 +0700

    [c] 183 - Add one more test and update .yml file

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,5 @@
+// Note Every test file should end with .test.js extension.
+
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
@@ -5,4 +7,11 @@ test('renders learn react link', () => {
   render(<App />)
   const linkElement = screen.getByText(/Bach Vu bookstore project/i)
   expect(linkElement).toBeInTheDocument()
+})
+
+const add = (a, b) => a + b
+
+test('function add should return the sum', () => {
+  const sum = add(3, 4)
+  expect(sum).toBe(7)
 })
