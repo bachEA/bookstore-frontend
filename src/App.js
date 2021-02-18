78d2b9a5e8f1455d907207c5afbb6e066318299d
PakVu <bach.x.vu@eastagile.com>
2021-02-18 10:10:14 +0700

    Add header and footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import logo from './logo.svg'
-import './App.css'
+import { Container } from 'react-bootstrap'
+
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <h1>Bach Vu bookstore project</h1>
-      </header>
+    <div>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Bach Vu bookstore project</h1>
+        </Container>
+      </main>
+
+      <Footer />
     </div>
   )
 }
