be165555bdaeba36e1224b9480b5090c20c0b16b
PakVu <bach.x.vu@eastagile.com>
2021-02-25 15:14:22 +0700

    fd

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,20 @@
 // Note Every test file should end with .test.js extension.
-import React from 'react'
-import ReactDOM from 'react-dom'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
 
-import { render, screen } from '@testing-library/react'
-import App from './App'
+// import { render, screen } from '@testing-library/react'
+// import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-})
+// it('renders without crashing', () => {
+//   const div = document.createElement('div')
+//   ReactDOM.render(<App />, div)
+// })
 
-test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/Bach Vu bookstore project/i)
-  expect(linkElement).toBeInTheDocument()
-})
+// test('renders learn react link', () => {
+//   render(<App />)
+//   const linkElement = screen.getByText(/Bach Vu bookstore project/i)
+//   expect(linkElement).toBeInTheDocument()
+// })
 
 const add = (a, b) => a + b
 
